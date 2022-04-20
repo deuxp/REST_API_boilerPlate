99567b6b194121692cf9ed38c95cc2f73055c5b5
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-04-20 00:07:26 -0400

    importing pkgs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import express from "express";
 import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
+const bodyParser = require("body-parser");
 
+// const ENV = process.env.NODE_ENV || 'developement'
 const app = express();
-
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json()); // access req.body
+// app.use(bodyParser.json()); // may not need
 app.use(morgan("dev"));
+app.use(cors());
+app.use(helmet());
 
 // endpoints
 app.get("/ping", (_req, res) => {
