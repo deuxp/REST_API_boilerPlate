2995d6fad34ff8d98cad43256d3ed021221bea70
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-04-20 15:28:19 -0400

    connect to db and simple query

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
+require("dotenv").config();
+const PORT = process.env.PORT || 8080;
 const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
-// const ENV = process.env.NODE_ENV || 'developement'
-const app = express();
-const PORT = process.env.PORT || 8080;
+const { Pool } = require("pg");
+let params = {};
+if (process.env.DATABASE_URL) {
+  params.connectionString = process.env.DATABASE_URL;
+} else {
+  params = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  };
+}
 
+const db = new Pool(params);
+db.connect();
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // access req.body
 // app.use(bodyParser.json()); // may not need
 app.use(morgan("dev"));
@@ -16,7 +34,14 @@ app.use(helmet());
 
 // endpoints
 app.get("/ping", (_req, res) => {
-  return res.send("<h1>PONG!</h1>");
+  const q = `SELECT * FROM messages`;
+  db.query(q)
+    .then((data) => {
+      return res.json(data.rows);
+    })
+    .catch((e) => {
+      res.send(e.message);
+    });
 });
 
 app.listen(PORT, () => {
